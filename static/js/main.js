63c3458774a80854d2e83c0e88b73d7c11bc37dd
Adrian Ko <adrianchko@gmail.com>
2014-12-03 18:41:22 +0000

    Some changes

--- static/js/main.js
+++ static/js/main.js
@@ -2,18 +2,18 @@ $(function() {
     console.log("\n\n\n\n\n\n\n");
 
     var photosets = [
-        "72157648127281331",
-        "72157648040800948"
+        "72157648490544211",
+        "72157648490550711"
     ];
 
-    var photos = [];
+    var photos = {};
     var url = "https://api.flickr.com/services/rest/?";
     var methods = {
         ps_photos: "flickr.photosets.getPhotos",
         ps_info: "flickr.photosets.getInfo",
         p_info: "flickr.photos.getInfo"
     };
-    var api_key = "3b99b0b8f427d265b121e1af5e251312";
+    var api_key = "0b2cf9d025573009ab21a8085dae6597";
 
     photosets.forEach(function(ps) {
         $.getJSON(
@@ -21,19 +21,31 @@ $(function() {
             function(json) {
                 var ps_photos = json.photoset.photo;
                 ps_photos.forEach(function(p) {
-                    $("#image").append(
-                        "<img src=\"http://farm3.staticflickr.com/"+p.server+"/"+p.id+"_"+p.secret+".jpg\" />"
-                    );
+                    photos[p.id] = {
+                        "image": p.id+"_"+p.secret
+                    };
 
                     $.getJSON(
                         url+"method="+methods.p_info+"&api_key="+api_key+"&photo_id="+ p.id+"&format=json&nojsoncallback=1",
                         function(pjson) {
-                            //pjson.photo.title._content;
-                            //pjson.photo.description._content;
+                            photos[p.id].title = pjson.photo.title._content;
+                            photos[p.id].description = pjson.photo.description._content;
+                            $("#image").prepend(
+                                "<div data-id=\""+p.id+"\">" +
+                                "<img src=\"http://farm3.staticflickr.com/"+p.server+"/"+photos[p.id].image+".jpg\" />" +
+                                "<div>"+photos[p.id].title+"</div>" +
+                                "<div>"+photos[p.id].description+"</div>" +
+                                "</div>" +
+                                "<br />"
+                            );
                         }
                     );
                 });
             }
         );
     });
+
+    $(document).ajaxStop(function() {
+        //load into window
+    })
 });
